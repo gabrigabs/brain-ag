9eebe6618a31f29251770c69121cb270a469d55c
Gabriel Bezerra Rodrigues <gabrielbrodrigues1@gmail.com>
2024-09-07 10:53:36 -0300

    feat: swagger module config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Rural Producers API')
+    .setDescription(
+      'An API that can manage rural producers farm data and also obtain detailed data with statistics on their farms',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
